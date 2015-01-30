df6a6ea8e2a1e5ed2c6ea5bb79d61b1206272b83
gabriel <gabriel@gabriel-HP-Pavilion-dv6-Notebook-PC>
2015-01-30 20:01:47 -0200

    Criei a classe Cliente.java

---
+++ src/Cliente.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author gabrielgaleazzi
+ *
+ */
+public class Cliente {
+
+	private String gabriel;
+
+	public String getGabriel() {
+		return gabriel;
+	}
+
+	public void setGabriel(String gabriel) {
+		this.gabriel = gabriel;
+	}
+	
+	
+}
